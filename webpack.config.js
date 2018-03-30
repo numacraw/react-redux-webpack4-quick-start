481c1732073b815305f212da26c37a1c72febb83
numacraw <qq172105117@gmail.com>
2018-03-30 11:24:20 +0800

    初始化

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
-const path = require('path');
-
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [ ]
 };
